7b6a87569af62604c4b733791573bb938b75a098
joaromera <joakromera@gmail.com>
2020-12-22 19:06:40 -0300

    set slider and make it visible for gain parameters

--- Source/GainPanel.cpp
+++ Source/GainPanel.cpp
@@ -16,6 +16,15 @@ GainPanel::GainPanel(OneDelayAudioProcessor* inProcessor, ODParameters inParamet
     , mSlider(mProcessor->parameters, inParameterID)
 {
     setSize(GAIN_PANEL_WIDTH, GAIN_PANEL_HEIGHT);
+    
+    const int sliderSize = 54;
+    
+    mSlider.setBounds(0.5f * (getWidth() - sliderSize),
+                      0.5f * (getHeight() - sliderSize),
+                      sliderSize,
+                      sliderSize);
+    
+    addAndMakeVisible(mSlider);
 }
 
 GainPanel::~GainPanel()
